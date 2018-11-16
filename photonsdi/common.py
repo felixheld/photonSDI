f7d542f77f3ea6bc06880d70da80e17cf2d4e806
Felix Held <felix-github@felixheld.de>
2018-11-16 15:35:03 +0100

    common.py: add comments

--- photonsdi/common.py
+++ photonsdi/common.py
@@ -1,4 +1,7 @@
+# common constants and defines for the SDI core
+
 SDI_ELEMENTARY_STREAM_DATA_WIDTH = 10
+# the 10 bit SDI data words are transmitted LSB first
 
 SDI_CRC_TAPS = [0, 4, 5, 18]
 SDI_CRC_LENGTH = max(SDI_CRC_TAPS)
