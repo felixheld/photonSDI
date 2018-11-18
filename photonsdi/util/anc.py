3b5ad06dfeb792a600fe9c26c08f2dbac180cfac
Felix Held <felix-github@felixheld.de>
2018-11-18 15:56:53 +0100

    anc.py: add util to en-/decode ancillary data header words

---
+++ photonsdi/util/anc.py
@@ -0,0 +1,36 @@
+from operator import xor
+
+from migen import *
+from photonsdi.constants import *
+
+
+class AncHeaderEncode(Module):
+    def __init__(self):
+        self.i_data = Signal(8)
+        self.o_data = Signal(SDI_ELEMENTARY_STREAM_DATA_WIDTH)
+
+        ###
+
+        parity = reduce(xor, [self.i_data[i] for i in range(8)])
+        self.comb += [
+            self.o_data.eq(Cat(self.i_data, parity, ~parity))
+        ]
+
+
+class AncHeaderDecode(Module):
+    def __init__(self):
+        self.i_data = Signal(SDI_ELEMENTARY_STREAM_DATA_WIDTH)
+        self.o_data = Signal(8)
+        self.o_valid = Signal()
+
+        ###
+
+        parity = reduce(xor, [self.i_data[i] for i in range(8)])
+        self.comb += [
+            self.o_data.eq(self.i_data[:8]),
+            If(self.i_data[8] == parity and self.i_data[9] == ~parity,
+                self.o_valid.eq(1)
+            ).Else(
+                self.o_valid.eq(0)
+            )
+        ]
