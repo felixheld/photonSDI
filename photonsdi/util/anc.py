1d5013fd59bd62368f08e0419e6796bad24f915c
Felix Held <felix-github@felixheld.de>
2018-11-18 16:27:31 +0100

    anc.py: use functionality from encdec.py

--- photonsdi/util/anc.py
+++ photonsdi/util/anc.py
@@ -2,6 +2,7 @@ from operator import xor
 
 from migen import *
 from photonsdi.constants import *
+from photonsdi.util.encdec import *
 
 
 class AncHeaderWordEncode(Module):
@@ -12,8 +13,12 @@ class AncHeaderWordEncode(Module):
         ###
 
         parity = reduce(xor, [self.i_data[i] for i in range(8)])
+
+        self.submodules.enc9 += Sdi9BitEncode()
+
         self.comb += [
-            self.o_data.eq(Cat(self.i_data, parity, ~parity))
+            self.enc9.i_data.eq(Cat(self.i_data, parity)),
+            self.o_data.eq(self.enc9.o_data)
         ]
 
 
@@ -25,10 +30,14 @@ class AncHeaderWordDecode(Module):
 
         ###
 
-        parity = reduce(xor, [self.i_data[i] for i in range(8)])
+        self.submodules.dec9 += Sdi9BitDecode()
+
+        parity = reduce(xor, [self.dec9.o_data[i] for i in range(8)])
+
         self.comb += [
-            self.o_data.eq(self.i_data[:8]),
-            If(self.i_data[8] == parity and self.i_data[9] == ~parity,
+            self.dec9.i_data.eq(self.i_data),
+            self.o_data.eq(self.dec9.o_data[:8]),
+            If(self.dec9.o_valid and self.self.dec9.o_data[8] == parity,
                 self.o_valid.eq(1)
             ).Else(
                 self.o_valid.eq(0)
