759dcd29a7ca9874d454d223fe96c4f225f151c9
Felix Held <felix-github@felixheld.de>
2018-11-18 16:16:11 +0100

    add en-/decode function for 9 bit aux data

---
+++ photonsdi/util/encdec.py
@@ -0,0 +1,37 @@
+from migen import *
+from photonsdi.constants import *
+
+
+# when 9 bit values are encoded for the SDI stream, the inverted 9th bit is
+# used as 10th bit so that the 10 bit value can't be outside the allowed data
+# value range
+
+
+class Sdi9BitEncode(Module):
+    def __init__(self):
+        self.i_data = Signal(9)
+        self.o_data = Signal(SDI_ELEMENTARY_STREAM_DATA_WIDTH)
+
+        ###
+
+        self.comb += [
+            self.o_data.eq(Cat(self.i_data, ~self.i_data[9]))
+        ]
+
+
+class Sdi9BitDecode(Module):
+    def __init__(self):
+        self.i_data = Signal(SDI_ELEMENTARY_STREAM_DATA_WIDTH)
+        self.o_data = Signal(9)
+        self.o_valid = Signal()
+
+        ###
+
+        self.comb += [
+            self.o_data.eq(self.i_data[:9]),
+            If(self.i_data[8] != self.i_data[9],
+                self.o_valid.eq(1)
+            ).Else(
+                self.o_valid.eq(0)
+            )
+        ]
